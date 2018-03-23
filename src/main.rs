822800729f0732ba2e75981a1f71b0638571d2ae
Martin Glagla <martingl@gmx.de>
2018-03-23 19:59:38 +0100

    added Options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate mp3_metadata;
+#[macro_use] extern crate structopt;
+extern crate glob;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Options {
+    #[structopt(short = "t", default_value = "%n - %a.mp3", long = "template")]
+    template: String,
+
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::from_args();
+    println!("{:?}", options);
 }
